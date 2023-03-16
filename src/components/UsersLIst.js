c582d94ba682809d2431b0fd90cb7ca107b7c405
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-03-16 16:07:55 +0000

    Added fetchUsers thunk

--- src/components/UsersLIst.js
+++ src/components/UsersLIst.js
@@ -1,7 +1,28 @@
-function UsersList () {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
 
+function UsersList() {
+
+    const dispatch = useDispatch();
+
+    const {data, isLoading, error} = useSelector((state) => {
+        return state.users;
+    })
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if (isLoading) { 
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>Error fetching data</div>
+    }
     return <div>
-        User's List
+        {data.length}
     </div>
 
 

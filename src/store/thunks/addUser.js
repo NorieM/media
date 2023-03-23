09a6178bf327959cb944549b0bc30c51d095788d
Norie <norie.macewan@tiscali.co.uk>
2023-03-23 17:03:15 +0000

    Added Thunk hook

--- src/store/thunks/addUser.js
+++ src/store/thunks/addUser.js
@@ -7,15 +7,8 @@ const addUser = createAsyncThunk('users/add', async () => {
   const response = await axios.post('http://localhost:3005/users', {
     name: faker.name.fullName()
   });
-  await pause(1000);
 
   return response.data;
 });
-
-const pause = (duration) => {
-  return new Promise((resolve)=> {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { addUser };
